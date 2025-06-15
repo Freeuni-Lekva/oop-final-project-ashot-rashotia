3f3e0988f34a152267b18c75490d69f3228182a2
Ika-Kvashali <ikvas22@freeuni.edu.ge>
2025-06-15 21:27:22 +0400

    Project Initialization

---
+++ QuizWebProject/src/test/java/com/example/quizwebproject/QuizWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizwebproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizWebProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
