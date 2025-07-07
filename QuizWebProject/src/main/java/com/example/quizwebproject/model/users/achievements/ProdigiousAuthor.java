e0bdfc81882765fab8a870a1e73961daf5dfae90
AgladzeTekla <tagla22@freeuni.edu.ge>
2025-07-08 02:50:44 +0400

    feat(author): add prodigious author skeleton class

---
+++ QuizWebProject/src/main/java/com/example/quizwebproject/model/users/achievements/ProdigiousAuthor.java
@@ -0,0 +1,8 @@
+package com.example.quizwebproject.model.users.achievements;
+
+public class ProdigiousAuthor extends Achievements {
+    @Override
+    public String getName() {
+        return "";
+    }
+}
