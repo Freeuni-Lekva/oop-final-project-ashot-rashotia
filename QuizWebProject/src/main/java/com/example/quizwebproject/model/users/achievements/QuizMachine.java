4e002d714044c52a11e312a3333b70ceaff1e838
AgladzeTekla <tagla22@freeuni.edu.ge>
2025-07-08 02:52:34 +0400

    feat(author): add quiz machine skeleton class

---
+++ QuizWebProject/src/main/java/com/example/quizwebproject/model/users/achievements/QuizMachine.java
@@ -0,0 +1,8 @@
+package com.example.quizwebproject.model.users.achievements;
+
+public class QuizMachine extends Achievements{
+    @Override
+    public String getName() {
+        return "";
+    }
+}
