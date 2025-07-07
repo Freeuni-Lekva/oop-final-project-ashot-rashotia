9c641cc524594cd74eaf4ebcf838396e0128d16c
AgladzeTekla <tagla22@freeuni.edu.ge>
2025-07-08 02:48:56 +0400

    feat(java): add practice makes perfect class

---
+++ QuizWebProject/src/main/java/com/example/quizwebproject/model/users/achievements/PracticeMakesPerfect.java
@@ -0,0 +1,11 @@
+package com.example.model.users.achievements;
+
+import jakarta.servlet.http.HttpServlet;
+
+public class PracticeMakesPerfect extends Achievements {
+
+    @Override
+    public String getName() {
+        return "practice makes perfect";
+    }
+}
