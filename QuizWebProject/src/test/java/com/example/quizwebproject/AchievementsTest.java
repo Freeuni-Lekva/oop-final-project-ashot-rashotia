7df29a2ae370fc1437cdfa809bb128e49638d231
AgladzeTekla <tagla22@freeuni.edu.ge>
2025-07-08 10:37:21 +0400

    test(achievement): add empty test functions for achievements

--- QuizWebProject/src/test/java/com/example/quizwebproject/AchievementsTest.java
+++ QuizWebProject/src/test/java/com/example/quizwebproject/AchievementsTest.java
@@ -1,4 +1,12 @@
 package com.example.quizwebproject;
 
 public class AchievementsTest {
+
+    void testCreateAchievementWithUser() {}
+    void testAchievementDateTimeIsSetOnCreate() {}
+    void testAmateurAuthorGetNameReturnsCorrectDescription() {}
+    void testAchievementsAreLinkedToUser() {}
+    void testFindUserAchievementsReturnsCorrectList() {}
+    void testGetRecentAchievementsPagination() {}
+    void testAchievementBehaviorWhenUserMissing() {}
 }
