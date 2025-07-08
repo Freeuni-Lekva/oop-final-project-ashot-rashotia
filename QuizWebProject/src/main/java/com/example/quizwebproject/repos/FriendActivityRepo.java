f3cff90b610a58c6a8552995f475942fba11ce4b
AgladzeTekla <tagla22@freeuni.edu.ge>
2025-07-08 21:11:26 +0400

    feat(repository): add FriendActivityRepo skeleton class

--- QuizWebProject/src/main/java/com/example/quizwebproject/repos/FriendActivityRepo.java
+++ QuizWebProject/src/main/java/com/example/quizwebproject/repos/FriendActivityRepo.java
@@ -1,4 +1,8 @@
 package com.example.quizwebproject.repos;
 
-public interface FriendActivityRepo {
+import com.example.quizwebproject.model.users.activities.FriendActivity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FriendActivityRepo extends JpaRepository<FriendActivity, Long> {
+    //
 }
