fcedb2ce5154420c0df22736ec577ee0ea00c2da
AgladzeTekla <tagla22@freeuni.edu.ge>
2025-07-08 10:21:13 +0400

    feat(repository): add findUserAchievements method to achievements repository

--- QuizWebProject/src/main/java/com/example/quizwebproject/repos/AchievementsRepo.java
+++ QuizWebProject/src/main/java/com/example/quizwebproject/repos/AchievementsRepo.java
@@ -1,7 +1,13 @@
 package com.example.quizwebproject.repos;
 
 import com.example.quizwebproject.model.users.achievements.Achievements;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AchievementsRepo extends JpaRepository<Achievements, Long> {
+    @Query("SELECT a FROM Achievements a WHERE a.user.id = :userId ORDER BY a.dateTime DESC")
+    List<Achievements> findUserAchievements(Long userId, Pageable pageable);
 }
