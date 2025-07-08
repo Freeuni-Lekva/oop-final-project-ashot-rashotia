6480c97300f20fe7bf918eb5e518b69ae6d49ff9
AgladzeTekla <tagla22@freeuni.edu.ge>
2025-07-08 09:48:46 +0400

    feat(model): add ProdigiousAuthor achievement model

--- QuizWebProject/src/main/java/com/example/quizwebproject/model/users/achievements/ProdigiousAuthor.java
+++ QuizWebProject/src/main/java/com/example/quizwebproject/model/users/achievements/ProdigiousAuthor.java
@@ -1,8 +1,21 @@
 package com.example.quizwebproject.model.users.achievements;
 
+import com.example.quizwebproject.model.users.User;
+import jakarta.persistence.Entity;
+
+@Entity
 public class ProdigiousAuthor extends Achievements {
+
+    public ProdigiousAuthor() {
+        super();
+    }
+
+    public ProdigiousAuthor(User user) {
+        super(user);
+    }
+
     @Override
     public String getName() {
-        return "";
+        return "Prodigious Author — The user created 10 or more quizzes.";
     }
 }
