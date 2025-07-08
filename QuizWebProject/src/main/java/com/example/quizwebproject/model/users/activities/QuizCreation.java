52c228fb3b677f6698e14c558eebac2e786a227c
AgladzeTekla <tagla22@freeuni.edu.ge>
2025-07-08 21:29:56 +0400

    feat(activity): implement simple methods in QuizCreation class

--- QuizWebProject/src/main/java/com/example/quizwebproject/model/users/activities/QuizCreation.java
+++ QuizWebProject/src/main/java/com/example/quizwebproject/model/users/activities/QuizCreation.java
@@ -1,4 +1,18 @@
 package com.example.quizwebproject.model.users.activities;
 
-public class QuizCreation {
+import jakarta.persistence.Entity;
+
+@Entity
+public class QuizCreation extends FriendActivity {
+
+    public QuizCreation() {}
+
+    public QuizCreation(Long userId, Long quizId, String username) {
+        super(userId, quizId, null, null, username);
+    }
+
+    @Override
+    public String getActivity() {
+        return "Create a new quiz";
+    }
 }
