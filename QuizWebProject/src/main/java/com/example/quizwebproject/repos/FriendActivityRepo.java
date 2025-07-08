3344531f304c025c9691017d657e65f7aa149a78
AgladzeTekla <tagla22@freeuni.edu.ge>
2025-07-08 22:04:56 +0400

    feat(repository): implement methods in FriendActivityRepo

--- QuizWebProject/src/main/java/com/example/quizwebproject/repos/FriendActivityRepo.java
+++ QuizWebProject/src/main/java/com/example/quizwebproject/repos/FriendActivityRepo.java
@@ -1,8 +1,15 @@
 package com.example.quizwebproject.repos;
 
 import com.example.quizwebproject.model.users.activities.FriendActivity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
 
 public interface FriendActivityRepo extends JpaRepository<FriendActivity, Long> {
-    //
+    @Query("SELECT fa FROM FriendActivity fa WHERE fa.userId = :userId AND fa.createdAt >= :cutoff ORDER BY fa.createdAt DESC")
+    Page<FriendActivity> findByUserId(@Param("userId") Long userId, @Param("cutoff")LocalDateTime cutoff, Pageable pageable);
 }
