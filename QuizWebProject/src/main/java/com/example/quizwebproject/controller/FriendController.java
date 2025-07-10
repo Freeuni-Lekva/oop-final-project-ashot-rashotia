185ce35312364edf90afa976138cc6e20ec89f3b
AgladzeTekla <tagla22@freeuni.edu.ge>
2025-07-10 16:50:17 +0400

    feat(friend): add send friend request function

--- QuizWebProject/src/main/java/com/example/quizwebproject/controller/FriendController.java
+++ QuizWebProject/src/main/java/com/example/quizwebproject/controller/FriendController.java
@@ -1,4 +1,38 @@
 package com.example.quizwebproject.controller;
 
+import com.example.quizwebproject.model.FriendRequest;
+import com.example.quizwebproject.model.users.User;
+import com.example.quizwebproject.service.FriendService;
+import com.example.quizwebproject.service.HomepageService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
 public class FriendController {
+    private final FriendService friendService;
+    private final HomepageService homepageService;
+
+    public FriendController(FriendService friendService, HomepageService homepageService) {
+        this.friendService = friendService;
+        this.homepageService = homepageService;
+    }
+
+    @PostMapping("/friend/send")
+    public String sendFriendRequest(@RequestParam("receiverId") Long receiverId, HttpSession session, RedirectAttributes redData) {
+        User sen = (User)session.getAttribute("user");
+
+        if (sen==null) {
+            return "redirect:/login";
+        }
+        FriendRequest req = friendService.sendFriendRequest(sen.getId(), receiverId);
+        if(req==null) {
+            redData.addAttribute("newFriendRequestSent", false);
+        } else {
+            redData.addAttribute("newFriendRequestSent", true);
+        }
+        return "redirect:/user/"+receiverId;
+    }
 }
